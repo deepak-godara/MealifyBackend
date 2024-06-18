24277b41a54825ee2635e5e656fc4b13bee58c69
anurag_singh722 <21bcs013@iiitdwd.ac.in>
2024-06-18 14:20:50 +0530

    deliverycode

--- backend/app.js
+++ backend/app.js
@@ -43,6 +43,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', '*');
   next();
 });
+
   io.on("connection",(socket)=>{
     console.log(socket.id);
     socket.on("disconnect",()=>{
