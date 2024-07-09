066dcc6fe64e14611c42f2254f4f24ebb11efa3e
Deepak Godara <godaradeepak318@gmail.com>
2024-07-09 20:45:22 +0530

    hi

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,25 @@
 const express = require("express");
 const http = require("http");
-const app = express();
 const path = require("path");
 const cors = require("cors");
-let scokets = [];
+const mongoose = require("mongoose");
 const { Server } = require("socket.io");
-const mongoose = require("mongoose"); // Corrected variable name
+
 const addHotelRoute = require("./routes/Hotel");
 const clientRoute = require("./routes/client");
 const ownerRoute = require("./routes/owner");
 const homeRoute = require("./routes/Home");
 const CartRoute = require("./routes/Cart");
-const SocketFunction = require("./utils/Sockets");
+const SocketFunction = require("./utils/Sockets").SocketFunction; // Import the SocketFunction
+
+const app = express();
+
 // Middleware for parsing JSON data
 app.use(express.json({ limit: "50mb" }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
@@ -45,12 +48,14 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "*");
   next();
 });
-  io.on("connection",(socket)=>{
-    console.log(socket.id);
-    socket.on("disconnect",()=>{
-       console.log("UserDisconnected",socket.id)
-    })
-  })
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+  SocketFunction(socket, io); // Call SocketFunction here
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
 
 app.use(homeRoute);
 app.use(addHotelRoute);
